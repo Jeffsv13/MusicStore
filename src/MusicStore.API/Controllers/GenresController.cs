ff220c85ca1b03ebb7947970f86ebcd8e299a440
Jeff Silva <jeffsilvavillarreal@gmail.com>
2025-01-21 21:16:31 -0500

    MS-03: Persistence technology was updated from memory to SQL Server, StandardAPI Responses and DTO Pattern applied while refactoring the code

--- src/MusicStore.API/Controllers/GenresController.cs
+++ src/MusicStore.API/Controllers/GenresController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MusicStore.Dto.Request;
+using MusicStore.Dto.Response;
 using MusicStore.Entities;
-using MusicStore.Repositories;
+using MusicStore.Repositories.Abstractions;
+using System.Net;
 
 namespace MusicStore.API.Controllers
 {
@@ -8,44 +11,110 @@ namespace MusicStore.API.Controllers
     [Route("api/genres")]
     public class GenresController : ControllerBase
     {
-        private readonly GenreRepository repository;
+        private readonly IGenreRepository repository;
+        private readonly ILogger<GenresController> logger;
 
-        public GenresController(GenreRepository repository)
+        public GenresController(
+            IGenreRepository repository,
+            ILogger<GenresController> logger)
         {
             this.repository = repository; 
+            this.logger = logger;
         }
 
         [HttpGet]
-        public ActionResult<List<Genre>> Get()
+        public async Task<IActionResult> Get()
         {
-            var data = repository.Get();
-            return Ok(data);
+            var response = new BaseResponseGeneric<List<GenreResponseDto>>();
+
+            try
+            {
+                response.Data = await repository.GetAsync();
+                response.Success = true;
+                logger.LogInformation("Obteniendo todos los generos musicales");
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = "Ocurrió un error al obtener la información";
+                logger.LogError(ex, $"{response.ErrorMessage} {ex.Message}");
+                return BadRequest(response);
+            }
         }
 
         [HttpGet("{id:int}")]
-        public ActionResult<Genre> Get(int id)
+        public async Task<IActionResult> Get(int id)
         {
-            var item = repository.Get(id);
-            return item is null ? NotFound() : Ok(item);
+            var response = new BaseResponseGeneric<GenreResponseDto>();
+            try
+            {
+                response.Data = await repository.GetAsync(id);
+                response.Success = true;
+                logger.LogInformation($"Obteniendo el genero musical con id {id}");
+                return response.Data == null ? NotFound() : Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = "Ocurrió un error al obtener la información";
+                logger.LogError(ex, $"{response.ErrorMessage} {ex.Message}");
+                return BadRequest(response);
+            }
         }
 
         [HttpPost]
-        public ActionResult<Genre> Post(Genre genre)
+        public async Task<IActionResult> Post(GenreRequestDto genre)
         {
-            repository.Add(genre);
-            return Ok(genre);
+            var response = new BaseResponseGeneric<int>();
+            try
+            {
+                var genreId = await repository.AddAsync(genre);
+                response.Data = genreId;
+                response.Success = true;
+                logger.LogInformation($"Género músical insertado con id {genreId}");
+                return StatusCode((int)HttpStatusCode.Created, response);
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = "Ocurrió un error al insertar la información";
+                logger.LogError(ex, $"{response.ErrorMessage} {ex.Message}");
+                return BadRequest(response);
+            }
         }
         [HttpPut("{id:int}")]
-        public ActionResult Put(int id, Genre genre)
+        public async Task<IActionResult> Put(int id, GenreRequestDto genre)
         {
-            repository.Update(id, genre);
-            return Ok();
+            var response = new BaseResponse();
+            try
+            {
+                await repository.UpdateAsync(id, genre);
+                response.Success = true;
+                logger.LogInformation($"Género músical actualizado con id {id}");
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = "Ocurrió un error al actualizar la información";
+                logger.LogError($"{response.ErrorMessage} {ex.Message}");
+                return BadRequest(response);
+            }
         }
         [HttpDelete("{id:int}")]
-        public ActionResult Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
-            repository.Delete(id);
-            return Ok();
+            var response = new BaseResponse();
+            try
+            {
+                await repository.DeleteAsync(id);
+                response.Success = true;
+                logger.LogInformation($"Género músical eliminado con id {id}");
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = "Ocurrió un error al eliminar la información";
+                logger.LogError($"{response.ErrorMessage} {ex.Message}");
+                return BadRequest(response);
+            }
         }
     }
 }
---
+++ src/MusicStore.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicStore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStore.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    //Fluent API
+    override protected void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        //Customizing the migration...
+        modelBuilder.Entity<Genre>().Property(x => x.Name).HasMaxLength(50);
+    }
+
+    //Entities to tables
+    public DbSet<Genre> Genres { get; set; }
+}
